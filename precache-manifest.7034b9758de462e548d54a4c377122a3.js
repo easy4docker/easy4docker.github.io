28805befa70f9140bccdacd2b3ebd8d24d9b874b
easy4docker <easy4docker@gmail.com>
2022-04-15 22:45:59 -0700

    update

---
+++ precache-manifest.7034b9758de462e548d54a4c377122a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe50e734e5d919ee98a6ccfb62d6227d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0cae7482845aa541a51",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "17033ecacdbf44609c2e",
+    "url": "/static/css/main.456f0205.chunk.css"
+  },
+  {
+    "revision": "d0cae7482845aa541a51",
+    "url": "/static/js/2.4343b69b.chunk.js"
+  },
+  {
+    "revision": "a700ee56574956102ab3",
+    "url": "/static/js/3.a5fac1fb.chunk.js"
+  },
+  {
+    "revision": "17033ecacdbf44609c2e",
+    "url": "/static/js/main.0a218271.chunk.js"
+  },
+  {
+    "revision": "6f13a377e55c7d6a65a7",
+    "url": "/static/js/runtime~main.67f928f1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.726fc3363a4570a46520ca9509b3bce7.js"
+  "/precache-manifest.7034b9758de462e548d54a4c377122a3.js"
 );
 
 self.addEventListener('message', (event) => {
