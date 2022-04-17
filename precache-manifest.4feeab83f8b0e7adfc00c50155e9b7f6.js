6b1443fe55ffe20c589caf369c943ad024fbe383
easy4docker <easy4docker@gmail.com>
2022-04-16 22:27:24 -0700

    update

---
+++ precache-manifest.4feeab83f8b0e7adfc00c50155e9b7f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35de0a52a27b59177ff61499ba5e2c12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d533fc00d5d0cea02473",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "5f3bfcf57223e5b66414",
+    "url": "/static/css/main.fd187fcd.chunk.css"
+  },
+  {
+    "revision": "d533fc00d5d0cea02473",
+    "url": "/static/js/2.297ace31.chunk.js"
+  },
+  {
+    "revision": "2d81513294b8db793e70",
+    "url": "/static/js/3.efc29aee.chunk.js"
+  },
+  {
+    "revision": "5f3bfcf57223e5b66414",
+    "url": "/static/js/main.d1510d22.chunk.js"
+  },
+  {
+    "revision": "d83dab676dc47b8d726e",
+    "url": "/static/js/runtime~main.ab2585a6.js"
+  }
+]);
